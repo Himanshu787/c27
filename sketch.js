4a095b26c6f52d792eb93d3a84a4089628b416dd
himanshu787 <nysayadav227@gmail.com>
2021-01-16 14:48:56 +0530

    created chain.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint=Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var constraintLog,chain;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +39,9 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    constraintLog = new Log(230,180,100,PI/2);
+    chain = new Chain(bird.body,constraintLog.body);
+
 }
 
 function draw(){
@@ -61,4 +67,7 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    chain.display();
+    constraintLog.display();
 }
